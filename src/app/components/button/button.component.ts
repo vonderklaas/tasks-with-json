4af4439829590fc057be482cbd97ad05f1df3679
Nick Garbalau <garbalaunick@protonmail.com>
2022-08-24 09:35:40 +0300

    Add custom and reusable button component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,8 +8,13 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ButtonComponent implements OnInit {
   @Input() color!: string;
   @Input() text!: string;
+  @Output() btnClick = new EventEmitter();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onClick() {
+    this.btnClick.emit();
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,4 +10,8 @@ export class HeaderComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  toggleAddTask() {
+    console.log('toggleAddTask');
+  }
 }
