b834262b970a7c98c135b0382889612961169fc9
Nick Garbalau <garbalaunick@protonmail.com>
2022-08-24 10:40:52 +0300

    Added json-server instead of mock data

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Task } from '../interfaces/Task';
-import { TASKS } from '../mock-tasks';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  private apiUrl = 'http://localhost:5000/tasks';
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
